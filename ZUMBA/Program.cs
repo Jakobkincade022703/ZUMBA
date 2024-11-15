1af588d0b7472b5b89353034dee0b0cab3ab4665
Jake Kincade <jakob.kincade@gmail.com>
2024-11-15 17:03:52 -0600

    Add project files.

---
+++ ZUMBA/Program.cs
@@ -0,0 +1,78 @@
+﻿/* Name: jakob kincade
+ * Section: CS258-00x
+ * Date: 11/15/2024
+ * Program description: 
+ */
+using System;
+
+namespace CS258
+{
+    class Zumba
+    {
+        // Data
+        // Weekly attendance (2D array)
+
+        // Daily attendance (1D array)
+
+        // Daily revenue (1D array)
+
+        // Weekly time-slot attendance (1D array)
+
+        // Weekly time-slot revenue (1D array)
+
+        // Methods
+        static void Main(string[] args)
+        {
+            // Create and initialize a Zumba class instance
+
+            // The do-while loop
+            // Textbook@P357
+            do
+            {
+                // Ask user if to show the attendance/income)
+
+                // Ask user if to continue the program
+
+                // To continue or break a loop
+                // Textbook@P368: break statement, Example 6-18
+                // Textbook@P369: continue statement
+            }
+            while (true);
+        }
+
+        public int GetWeeklyAttendance()
+        {
+            
+        }
+
+        public int[] GetPerSlotAttendance()
+        {
+
+        }
+
+        public int[] GetPerDayAttendance()
+        {
+
+        }
+
+        public int GetWeeklyRevenue()
+        {
+
+        }
+
+        public int[] GetPerSlotRevenue()
+        {
+
+        }
+
+        public int[] GetPerDayRevenue()
+        {
+
+        }
+
+        public void Display()
+        {
+
+        }
+    }
+}
